e3e79c91aaf1c65fdf80409c592b8e82bdef1ea6
Husnain-7 <husnainbinashraf7001341@gmail.com>
2025-09-22 13:06:46 +0500

    Update quivo_shipment_webhook.php

--- quivo_shipment_webhook.php
+++ quivo_shipment_webhook.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // DB connection
-$mysqli = new mysqli("localhost", "apidoctorz_bclix", "bclix@768", "apidoctorz_funnel-tunnel");
+$mysqli = new mysqli("localhost", "u534957383_husnain7z", "4876246@Hostinger", "u534957383_funnel_tunnel");
 if ($mysqli->connect_error) {
     http_response_code(500);
     die("DB Connection failed: " . $mysqli->connect_error);
@@ -12,11 +12,23 @@ if ($mysqli->connect_error) {
 
 // Get raw webhook payload
 $rawPayload = file_get_contents("php://input");
+$data = json_decode($rawPayload, true);
+if (!$data) {
+    http_response_code(400);
+    die(json_encode(["status"=>"error","message"=>"Invalid JSON"]));
+}
+
 
 // Save raw payload into DB
 $stmt = $mysqli->prepare("INSERT INTO shipment_data (payload) VALUES (?)");
 $stmt->bind_param("s", $rawPayload);
-$stmt->execute();
+
+if (!$stmt->execute()) {
+    http_response_code(500);
+    echo json_encode(["status" => "error", "message" => $stmt->error]);
+    exit;
+}
+
 $stmt->close();
 $mysqli->close();
 
